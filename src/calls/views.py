dc085102b7e239ce48c4a4b97afe05710844769d
Gabriel Bueno <lbueno.gabriel@gmail.com>
2021-10-01 23:51:42 -0300

    Search

--- src/calls/views.py
+++ src/calls/views.py
@@ -4,7 +4,17 @@ from categories.models import Category
 from status.models import Status
 from priorities.models import Priority
 from nerds.models import Nerd
-from django.contrib.auth.models import User
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+# Config server to send the email with response
+host = '<HOST_URL>'
+port = 587
+user = '<USER>'
+password = '<PASS>'
+
+
 
 # Create your views here.
 
@@ -29,8 +39,29 @@ def calls_detail_view(request, id):
             if request.POST.get('nerdCall',False):
                 call.nerdCall = request.POST['nerdCall']
 
-
         call.save()
+
+
+        if call.emailResponseCall:
+            # the object
+            server = smtplib.SMTP(host, port)
+            # Login server
+            server.ehlo()
+            server.starttls()
+            server.login(user, password)
+
+            # Creating the message
+            message = 'Hello, world!'
+
+            email_msg = MIMEMultipart()
+            email_msg['From'] = user
+            email_msg['To'] = '<EMAIL_DE_DESTINO>'
+            email_msg['Subject'] = 'message subj'
+            email_msg.attach(MIMEText(message, 'plain'))
+
+            # Sending the message
+            server.sendmail(email_msg['From'], email_msg['To'], email_msg.as_string())
+            server.quit()
 
     nerd = Nerd.objects.get(nerdLogin=call.nerdCall)
     context = {
@@ -83,6 +114,27 @@ def mycalls_detail_view(request, id):
 
         mycall.save()
 
+        if mycall.emailResponseCall:
+            # the object
+            server = smtplib.SMTP(host, port)
+            # Login server
+            server.ehlo()
+            server.starttls()
+            server.login(user, password)
+
+            # Creating the message
+            message = 'Hello, world!'
+
+            email_msg = MIMEMultipart()
+            email_msg['From'] = user
+            email_msg['To'] = '<EMAIL_DE_DESTINO>'
+            email_msg['Subject'] = 'message subj'
+            email_msg.attach(MIMEText(message, 'plain'))
+
+            # Sending the message
+            server.sendmail(email_msg['From'], email_msg['To'], email_msg.as_string())
+            server.quit()
+
     nerd = Nerd.objects.get(nerdLogin=mycall.nerdCall)
     context = {
         "mycall": Call.objects.get(id=id),
@@ -113,8 +165,6 @@ def mycalls_list_view(request, *args, **kwargs):
     return render(request, "mycalls/list.html", context)
 
 def calls_list_search_view(request, *args, **kwargs):
-    # nerd = request.user.username
-
 
     calls = Call.objects.all()
     if request.method == 'GET':
@@ -124,22 +174,20 @@ def calls_list_search_view(request, *args, **kwargs):
 
 
         if int(status) != 0:
-            calls.filter(statusCall=status)
+            calls = calls.filter(statusCall=status)
 
         if int(prior) != 0:
-            calls.filter(priorityCall=prior)
+            calls = calls.filter(priorityCall=prior)
 
         if int(categ) != 0:
-            calls.filter(categoryCall=categ)
- 
-
+            calls = calls.filter(categoryCall=categ)
 
         context = {
-        "calls": calls,
-        "status": Status.objects.all(),
-        "priorities": Priority.objects.all(),
-        "categories": Category.objects.all(),
-        # "nerdName": nerdName
+            "calls": calls,
+            "status": Status.objects.all(),
+            "priorities": Priority.objects.all(),
+            "categories": Category.objects.all()
+ 
         }
 
 
@@ -154,34 +202,29 @@ def mycalls_list_search_view(request, *args, **kwargs):
 
     mycalls = Call.objects.filter(nerdCall=nerd)
 
-    context = {
-        "mycalls": mycalls,
-        "status": Status.objects.all(),
-        "priorities": Priority.objects.all(),
-        "categories": Category.objects.all(),
-        # "nerdName": nerdName
-        }
-
     if request.method == 'GET':
         status = request.GET.get('statusCall')
         prior = request.GET.get('priorityCall')
         categ = request.GET.get('categoryCall')
 
 
-
         if int(status) != 0:
-            context['status'] = Status.objects.filter(id=status)
-
+            mycalls = mycalls.filter(statusCall=status)
 
         if int(prior) != 0:
-            context['priorities'] = Priority.objects.filter(id=prior)
-
+            mycalls = mycalls.filter(priorityCall=prior)
 
         if int(categ) != 0:
-            context['categories'] = Category.objects.filter(id=categ)
+            mycalls = mycalls.filter(categoryCall=categ)
 
 
+    context = {
+        "mycalls": mycalls,
+        "status": Status.objects.all(),
+        "priorities": Priority.objects.all(),
+        "categories": Category.objects.all(),
 
+        }
 
 
     return render(request, "mycalls/list.html", context)
